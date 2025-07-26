3ce9c526fb7358848d3ca7dd0dacf5376b60b676
Ifaz Howlader <ifaz.howl@gmail.com>
2025-07-26 16:30:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputEl = document.getElementById('input-el')
+let saveEl = document.getElementById('save-el')
+let deleteEl = document.getElementById('delete-el')
+let tabEl = document.getElementById('tab-el')
+let listEl = document.getElementById('list-el')
+let links = []
+
+
